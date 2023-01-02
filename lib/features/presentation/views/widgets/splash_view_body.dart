9815b4e293b6ba52a1c6e799351a79afa25a1f7d
Sami <sami.amin.6.2001@gmail.com>
2023-01-03 01:49:01 +0200

    animated text splash

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,43 @@
 import 'package:bookly/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+
+    animationController.forward();
+    // slidingAnimation.addListener(() {  // we don't need setState becaue we do AnimatedBuilder to the widget
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +52,32 @@ class SplashViewBody extends StatelessWidget {
           const SizedBox(
             height: 8,
           ),
-          const Text(
-            'read free box',
-            textAlign: TextAlign.center,
-          ),
+          SlidingText(slidingAnimation: slidingAnimation),
         ],
+      ),
+    );
+  }
+}
+
+//move this to dir widget
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    Key? key,
+    required this.slidingAnimation,
+  }) : super(key: key);
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) => SlideTransition(
+        position: slidingAnimation,
+        child: const Text(
+          'read free box',
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
