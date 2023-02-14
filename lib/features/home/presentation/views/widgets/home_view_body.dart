6f23beadcd2041f0a944943cca13baeda9a4cb3d
Sami <sami.amin.6.2001@gmail.com>
2023-02-14 02:17:13 +0200

    best seller item part 3

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,12 +1,8 @@
-import 'dart:ui';
-
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/assets_data.dart';
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
 import 'featured_list_view.dart';
+import 'item_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -37,44 +33,3 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class ItemListView extends StatelessWidget {
-  const ItemListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red,
-                image: const DecorationImage(
-                    image: AssetImage(AssetsData.logo), fit: BoxFit.fill),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  "Harry Poter And The Goblet Of Fire  Harry Poter And The Goblet Of Fire",
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
---
+++ lib/features/home/presentation/views/widgets/item_list_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets_data.dart';
+import '../../../../../core/utils/styles.dart';
+
+class ItemListView extends StatelessWidget {
+  const ItemListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                    image: AssetImage(AssetsData.logo), fit: BoxFit.fill),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  "Harry Poter And The Goblet Of Fire  Harry Poter And The Goblet Of Fire",
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+               const Text("J.K Sami Amin",style: Styles.textStyle14,),
+               const SizedBox(
+                height: 3,
+              ),
+              Text("19.99 \$ ",style: Styles.textStyle20.copyWith(
+                fontWeight: FontWeight.bold
+              ),),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
