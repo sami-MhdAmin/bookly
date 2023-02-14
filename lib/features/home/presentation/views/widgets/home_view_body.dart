b499acece4401264de025177ff6b6da7ce0211c3
Sami <sami.amin.6.2001@gmail.com>
2023-02-14 17:29:25 +0200

    best seller with custom scroll view done

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'best_seller_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 import 'item_list_view.dart';
@@ -9,26 +10,41 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(
-            height: 30,
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: CustomAppBar(),
+              ),
+              FeaturedBooksListView(),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
+        ),
+        //you can use sliver adapter but with shrink in list..and this is bad
+        const SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          ItemListView()
-        ],
-      ),
+        )
+      ],
     );
   }
 }
--- lib/features/home/presentation/views/widgets/item_list_view.dart
+++ lib/features/home/presentation/views/widgets/item_list_view.dart
@@ -27,34 +27,42 @@ class ItemListView extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  "Harry Poter And The Goblet Of Fire  Harry Poter And The Goblet Of Fire",
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: const Text(
+                    "Harry Poter And The Goblet Of Fire  Harry Poter And The Goblet Of Fire",
+                    style: Styles.textStyle20,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              const Text(
-                "J.K Sami Amin",
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                "19.99 \$ ",
-                style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
-              ),
-              const RatingBar()
-            ],
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  "J.K Sami Amin",
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "19.99 \$ ",
+                      style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    const RatingBar()
+                  ],
+                ),
+                
+              ],
+            ),
           )
         ],
       ),
