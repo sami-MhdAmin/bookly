6d1a96905427372fccbf1e1acb85cfd77a976f8a
Sami <sami.amin.6.2001@gmail.com>
2023-03-03 01:10:29 +0200

    create custom serach textField

---
+++ lib/features/search/presentation/view/widget/search_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'custom_search_textfield.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: const [
+          CustomSearchTextField(),
+        ],
+      ),
+    );
+  }
+}
